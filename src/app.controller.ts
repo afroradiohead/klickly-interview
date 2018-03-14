500d0717db6c4d8e2bed57ea403e6b3ff95e5d47
Christopher Clarke <afroradiohead@gmail.com>
2018-03-14 14:57:13 -0700

    add homepage

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,12 @@ import {Get, Controller, Req, Res} from '@nestjs/common';
 @Controller()
 export class AppController {
 	@Get()
-	root(@Res() response) {
-
+	root() {
+		return `
+		<form action="/api/migrate" method="POST">
+			<label>Store Name</label> <input name="storeName" type="text" />
+			<button type="submit">Migrate</button>
+		</form>
+		`;
   }
 }
